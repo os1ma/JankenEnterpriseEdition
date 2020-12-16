83b91cbb5673016f81b9ac5d08e0fe56d8b593c4
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2020-12-16 19:16:42 +0900

    Bean Validation を使うよう修正

--- app/src/main/java/com/example/janken/presentation/api/janken/JankenAPIController.java
+++ app/src/main/java/com/example/janken/presentation/api/janken/JankenAPIController.java
@@ -3,6 +3,7 @@ package com.example.janken.presentation.api.janken;
 import com.example.janken.application.service.JankenApplicationService;
 import lombok.AllArgsConstructor;
 import lombok.val;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ public class JankenAPIController {
     private JankenApplicationService service;
 
     @PostMapping
-    public JankenPostResponseBody post(@RequestBody JankenPostRequestBody requestBody) {
+    public JankenPostResponseBody post(@RequestBody @Validated JankenPostRequestBody requestBody) {
 
         val maybeWinner = service.play(
                 requestBody.getPlayer1Id(),
